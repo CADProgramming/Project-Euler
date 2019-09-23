245bb760d349e8f2ac2d15f164c2aa8fdad72f71
davica3 <davica3@D312-54576>
2019-09-23 15:16:40 +1200

    Challenge 2

---
+++ Challenge 2/Challenge 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int last = 1;
+            int current = 1;
+
+            while ((current + last) < 4000000)
+            {
+                int newLast = current;
+                current += last;
+                last = newLast;
+
+                if (current % 2 == 0)
+                {
+                    total += current;
+                }
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
