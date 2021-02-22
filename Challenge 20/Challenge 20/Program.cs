8d2e357b7d6ba5448327f0e8809aadc909173785
Clayton Davidson <casd693@gmail.com>
2021-02-23 00:44:46 +1300

    Completed challenge 20

--- Challenge 20/Challenge 20/Program.cs
+++ Challenge 20/Challenge 20/Program.cs
@@ -6,7 +6,36 @@ namespace Challenge_20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int NUMBER = 100;
+            long[] placeValues = new long[165];
+            placeValues[placeValues.Length - 1] = 1;
+
+            for (int i = NUMBER; i > 0; i--)
+            {
+                for (int n = placeValues.Length - 1; n >= 0; n--)
+                {
+                    placeValues[n] *= i;
+                }
+
+                for (int n = placeValues.Length - 1; n >= 0; n--)
+                {
+                    if (placeValues[n] > 9)
+                    {
+                        placeValues[n - 1] += placeValues[n] / 10;
+                        placeValues[n] %= 10;
+                    }
+                }
+            }
+
+            long digitSum = 0;
+
+            for (int i = 0; i < placeValues.Length; i++)
+            {
+                digitSum += placeValues[i];
+            }
+
+            Console.WriteLine(digitSum);
+            Console.ReadLine();
         }
     }
 }
